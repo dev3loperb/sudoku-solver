1b892ecaf77590ac21ca0738987dcaa94593a2dd
Igor Pergenitsa <ipergenitsa@gmail.com>
2017-08-07 03:25:18 +0600

    refactoring

--- src/main/scala/com/github/ipergenitsa/sudoku/solver/Bootstrap.scala
+++ src/main/scala/com/github/ipergenitsa/sudoku/solver/Bootstrap.scala
@@ -10,7 +10,7 @@ object Bootstrap extends App {
 
   val lines = Source.fromResource("input.txt").getLines().toSeq
 
-  var board: Board = for (line <- lines) yield
+  val board: Board = for (line <- lines) yield
     for (symbol <- line) yield {
       if (symbol.asDigit == 0) {
         new EmptyCell
@@ -18,42 +18,18 @@ object Bootstrap extends App {
         new ValueCell(symbol.asDigit)
       }
     }
-  for (k <- 0 until 10) {
-    for (i <- 0 until width) {
-      for (j <- 0 until height) {
-        board(i)(j) match {
-          case value: ValueCell =>
-            board = Utils.fill(board, value.number, i, j)
-          case _ =>
-        }
-      }
-    }
 
-    for (i <- 0 until width) {
-      for (j <- 0 until height) {
-        board(i)(j) match {
-          case emptyCell: EmptyCell =>
-            if (emptyCell.notValues.size == 8) {
-              val a: Int = (1 to 9).toSet.diff(emptyCell.notValues).head
-              board = board.updated(i, board(i).updated(j, new ValueCell(a)))
-            }
-          case _ =>
-        }
-      }
-    }
-  }
+  val resolved = Game.resolve(board)
 
-  board.zipWithIndex.foreach {
-    case (row, rIndex) => {
+  resolved.zipWithIndex.foreach {
+    case (row, rIndex) =>
       row.zipWithIndex.foreach {
-        case (cell, cIndex) => {
-          println(s"cell: $rIndex; $cIndex. Value: ${cell.getValue}. Non Values: ${cell.getNonValues}" )
-        }
+        case (cell, cIndex) =>
+          println(s"cell: $rIndex; $cIndex. Value: ${cell.getValue}. Non Values: ${cell.getNonValues}")
       }
-    }
   }
 
-  Utils.printBoard(board)
+  Utils.printBoard(resolved)
 }
 
 object Game {
@@ -67,31 +43,75 @@ object Game {
     override def addNotValue(notValue: Int): MultiLayerCell = new EmptyCell(notValues + notValue)
     override def getValue: String = "*"
     override def getNonValues: Set[Int] = notValues
+
+    override def equals(obj: scala.Any): Boolean = obj match {
+      case that: EmptyCell => notValues.equals(that.notValues)
+      case _ => false
+    }
   }
   class ValueCell(val number: Int, val notValues: Set[Int] = Set()) extends MultiLayerCell {
     override def addNotValue(notValue: Int) = new ValueCell(number, notValues + notValue)
     override def getValue: String = number.toString
     override def getNonValues: Set[Int] = notValues
+
+    override def equals(obj: scala.Any): Boolean = obj match {
+      case that: ValueCell => number.equals(that number) && notValues.equals(that notValues)
+      case _ => false
+    }
+  }
+
+  def resolve(board: Board): Board = {
+    var nextBoard = nextStep(board)
+    while (!nextStep(nextBoard).equals(nextBoard)) {
+      nextBoard = nextStep(nextBoard)
+    }
+    nextBoard
+  }
+
+  def nextStep(board: Board): Board = {
+    resolveNotValues(fillInNotValues(board))
+  }
+
+  def fillInNotValues(board: Board): Board = {
+    var filledBoard: Board = board
+    for (i <- 0 until 9; j <- 0 until 9) {
+      filledBoard(i)(j) match {
+        case value: ValueCell =>
+          filledBoard = Utils.fill(filledBoard, value.number, i, j)
+        case _ =>
+      }
+    }
+    filledBoard
+  }
+
+  private def resolveNotValues(board: Board): Board = {
+    var updatedBoard: Board = board
+    for (i <- 0 until 9; j <- 0 until 9) {
+      updatedBoard(i)(j) match {
+        case emptyCell: EmptyCell =>
+          if (emptyCell.notValues.size == 8) {
+            val a: Int = (1 to 9).toSet.diff(emptyCell.notValues).head
+            updatedBoard = updatedBoard.updated(i, updatedBoard(i).updated(j, new ValueCell(a)))
+          }
+        case _ =>
+      }
+    }
+    updatedBoard
   }
 }
 
 object Utils {
-  def fill(board: Board, value: Int, x: Int, y: Int) = {
-    var updatedBoard = board.updated(x, board(x).zipWithIndex.map { case (cell, index) => {
-      if (index != y) {
-        cell.addNotValue(value)
-      } else {
-        cell
-      }
-    }
+  def fill(board: Board, value: Int, x: Int, y: Int): Board = {
+    fillRow(board, value, x).map(row => {
+      row.updated(y, row(y).addNotValue(value))
     })
-    for (i <- 0 until 9; if i != y) {
-      updatedBoard = updatedBoard.updated(i, updatedBoard(i).updated(y, updatedBoard(i)(y).addNotValue(value)))
-    }
-    updatedBoard
+  }
+
+  private def fillRow(board: Board, value: Int, x: Int): Board = {
+    board.updated(x, board(x).map(_.addNotValue(value)))
   }
 
   def printBoard(board: Board): Unit = {
-    board.foreach(row => println(row.map(_.getValue).mkString(" ")))
+    board.foreach(row => println(row.map(_.getValue).mkString("")))
   }
 }
