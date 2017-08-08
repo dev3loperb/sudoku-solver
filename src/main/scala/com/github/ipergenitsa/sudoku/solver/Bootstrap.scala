42befa228f0e63337ca70acf1a0af6e421761f76
Igor Pergenitsa <ipergenitsa@gmail.com>
2017-08-08 17:46:26 +0600

    change type of Board from Seq[Seq] to Map

--- src/main/scala/com/github/ipergenitsa/sudoku/solver/Bootstrap.scala
+++ src/main/scala/com/github/ipergenitsa/sudoku/solver/Bootstrap.scala
@@ -5,55 +5,53 @@ import com.github.ipergenitsa.sudoku.solver.Game.{Board, EmptyCell, ValueCell}
 import scala.io.Source
 
 object Bootstrap extends App {
-  val width = 9
-  val height = 9
+  object const {
+    val size = 9
+  }
 
   val lines = Source.fromResource("input.txt").getLines().toSeq
 
-  val board: Board = for (line <- lines) yield
-    for (symbol <- line) yield {
+  var board: Board = Map()
+  for ((line, lineIndex) <- lines.zipWithIndex;
+                          (symbol, symbolIndex) <- line.zipWithIndex) {
+    val point = Game.Point(lineIndex, symbolIndex)
       if (symbol.asDigit == 0) {
-        new EmptyCell
+        board = board + (point -> new EmptyCell)
       } else {
-        new ValueCell(symbol.asDigit)
+        board = board + (point -> new ValueCell(symbol.asDigit))
       }
     }
 
   val resolved = Game.resolve(board)
 
-  resolved.zipWithIndex.foreach {
-    case (row, rIndex) =>
-      row.zipWithIndex.foreach {
-        case (cell, cIndex) =>
-          println(s"cell: $rIndex; $cIndex. Value: ${cell.getValue}. Non Values: ${cell.getNonValues}")
-      }
-  }
-
   Utils.printBoard(resolved)
 }
 
 object Game {
-  type Board = Seq[Seq[MultiLayerCell]]
+  case class Point(x: Int, y: Int)
+
+  type Board = Map[Point, MultiLayerCell]
+
   trait MultiLayerCell {
     def addNotValue(notValue: Int): MultiLayerCell
     def getValue: String
     def getNonValues: Set[Int]
   }
+
   class EmptyCell(val notValues: Set[Int] = Set()) extends MultiLayerCell {
     override def addNotValue(notValue: Int): MultiLayerCell = new EmptyCell(notValues + notValue)
     override def getValue: String = "*"
     override def getNonValues: Set[Int] = notValues
-
     override def equals(obj: scala.Any): Boolean = obj match {
       case that: EmptyCell => notValues.equals(that.notValues)
       case _ => false
     }
   }
+
   class ValueCell(val number: Int, val notValues: Set[Int] = Set()) extends MultiLayerCell {
     override def addNotValue(notValue: Int) = new ValueCell(number, notValues + notValue)
     override def getValue: String = number.toString
     override def getNonValues: Set[Int] = notValues
-
     override def equals(obj: scala.Any): Boolean = obj match {
       case that: ValueCell => number.equals(that number) && notValues.equals(that notValues)
       case _ => false
@@ -75,7 +73,7 @@ object Game {
   def fillInNotValues(board: Board): Board = {
     var filledBoard: Board = board
     for (i <- 0 until 9; j <- 0 until 9) {
-      filledBoard(i)(j) match {
+      filledBoard(Game.Point(i, j)) match {
         case value: ValueCell =>
           filledBoard = Utils.fill(filledBoard, value.number, i, j)
         case _ =>
@@ -87,11 +85,11 @@ object Game {
   private def resolveNotValues(board: Board): Board = {
     var updatedBoard: Board = board
     for (i <- 0 until 9; j <- 0 until 9) {
-      updatedBoard(i)(j) match {
+      updatedBoard(Game.Point(i, j)) match {
         case emptyCell: EmptyCell =>
           if (emptyCell.notValues.size == 8) {
             val a: Int = (1 to 9).toSet.diff(emptyCell.notValues).head
-            updatedBoard = updatedBoard.updated(i, updatedBoard(i).updated(j, new ValueCell(a)))
+            updatedBoard = updatedBoard.updated(Game.Point(i, j), new ValueCell(a))
           }
         case _ =>
       }
@@ -102,16 +100,26 @@ object Game {
 
 object Utils {
   def fill(board: Board, value: Int, x: Int, y: Int): Board = {
-    fillRow(board, value, x).map(row => {
-      row.updated(y, row(y).addNotValue(value))
-    })
+    fillAux(board, value, x, y, 0)
   }
 
-  private def fillRow(board: Board, value: Int, x: Int): Board = {
-    board.updated(x, board(x).map(_.addNotValue(value)))
+  private def fillAux(board: Board, value: Int, x: Int, y: Int, step: Int): Board = {
+    if (step > Bootstrap.const.size - 1 || step < 0) {
+      board
+    } else {
+      val xKey = Game.Point(x, step)
+      val yKey = Game.Point(step, y)
+      val updatedBoard = board.updated(xKey, board(xKey).addNotValue(value))
+        .updated(yKey, board(yKey).addNotValue(value))
+      fillAux(updatedBoard, value, x, y, step + 1)
+    }
   }
 
   def printBoard(board: Board): Unit = {
-    board.foreach(row => println(row.map(_.getValue).mkString("")))
+    for (i <- 0 until Bootstrap.const.size) {
+      val row = (0 until Bootstrap.const.size).map(j => board(Game.Point(i, j)))
+        .map(_.getValue).mkString("")
+      println(row)
+    }
   }
 }
